3017bcbc78a85423ce10513a6a2d29911ac70d5d
MackllaussIcomp <dmcc@icomp.ufam.edu.br>
2025-06-16 19:00:44 -0400

    Copia de Imagem2

--- Aula_04.cpp
+++ Aula_04.cpp
@@ -8,13 +8,16 @@ int main() {
 
     //Open source file in binary
     std::ifstream image(inputPath, std::ios::binary);
-   /*  if (!image) {
+    
+    //DEBUG
+    /*  if (!image) {
         std::cerr << "Error: Unable to open source file: " << inputPath << '\n';
         return 1;
     } */
 
     //Open destination file in binary
     std::ofstream newImage(outputPath, std::ios::binary);
+   //DEBUG
    /*  if (!newImage) {
         std::cerr << "Error: Unable to create destination file: " << outputPath << '\n';
         return 1;
@@ -24,7 +27,7 @@ int main() {
     image.seekg(0, std::ios::end);
     std::streamsize size = image.tellg();
     image.seekg(0, std::ios::beg);
-
+    //DEBUG
     /* if (size <= 0) {
         std::cerr << "Error: Invalid file size.\n";
         return 1;
@@ -32,7 +35,8 @@ int main() {
 
     //Allocate memory buffer
     std::unique_ptr<char[]> buffer(new(std::nothrow) char[size]);
-  /*   if (!buffer) {
+    //DEBUG
+    /* if (!buffer) {
         std::cerr << "Error: Memory allocation failed.\n";
         return 1;
     } */
@@ -43,7 +47,7 @@ int main() {
         return 1;
     }
  */
-    //Write buffer to output
+
     newImage.write(buffer.get(), size);
   /*   if (!newImage) {
         std::cerr << "Error: Failed to write to destination file.\n";
